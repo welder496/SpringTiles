a42a9521fb4335020ff11a3340de76bc001cd6a3
welder496 <welder496@gmail.com>
2016-06-22 10:57:58 -0300

    Adição de arquivos de configuração e inicialização do spring

---
+++ src/main/java/br/jus/cnj/configuration/SpringInitialization.java
@@ -0,0 +1,24 @@
+package br.jus.cnj.configuration;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class SpringInitialization extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return new Class[] {SpringConfiguration.class};
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] { "/" };
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>Spring Basic Application</h2>
+</body>
+</html>
