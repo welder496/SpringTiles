5e7270e072304941b7166e55cef40e670e15ff40
welder496 <welder496@gmail.com>
2016-06-29 12:57:52 -0300

    Adição de tela de login e de acesso não permitido!!

--- src/main/java/br/jus/cnj/controller/ConvidadoController.java
+++ src/main/java/br/jus/cnj/controller/ConvidadoController.java
@@ -1,9 +1,15 @@
 package br.jus.cnj.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.ui.Model;
@@ -43,6 +49,20 @@ public class ConvidadoController {
         }		
 	}
 	
+	@RequestMapping(value="/login", method = RequestMethod.GET)
+	public String loginPage(){
+		return "login";
+	}
+	
+	@RequestMapping(value="/logout", method = RequestMethod.GET)
+	public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+	    Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    if (auth != null){    
+	        new SecurityContextLogoutHandler().logout(request, response, auth);
+	    }
+	    return "redirect:/login?logout";
+	}
+	
 	@RequestMapping(value="/atualizar", method=RequestMethod.POST)
 	public String atualizar(@Valid @ModelAttribute("convidado") Convidado convidado, BindingResult result, Model model){
 		if (! result.hasErrors()){
@@ -67,6 +87,13 @@ public class ConvidadoController {
 		model.addAttribute("convidado", convidado);
 		return "mostrarEdicao";
 	}
+	
+	@RequestMapping(value = "/Access_Denied", method = RequestMethod.GET)
+	public String accessDeniedPage(Model model) {
+		model.addAttribute("user", getPrincipal());
+		return "accessDenied";
+	}	
+	
 	
 	@RequestMapping(value="/excluir/{codigo}", method=RequestMethod.GET)
 	public String remover(@PathVariable("codigo") int codigo, Model model){
@@ -112,6 +139,18 @@ public class ConvidadoController {
 	@RequestMapping(value={"/","/index"}, method=RequestMethod.GET)
 	public String getPhrase(){
 		return "index";
+	}
+	
+	private String getPrincipal(){
+		String userName = null;
+		Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		
+		if (principal instanceof UserDetails){
+			userName = ((UserDetails)principal).getUsername();
+		} else {
+			userName = principal.toString();
+		}
+		return userName;
 	}
 	
 }
